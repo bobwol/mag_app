b942ace17b015b41e14fa89fc4e38a5267df65a0
Prelang Builder <builder@prelang.com>
2016-04-14 02:20:23 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Subscription' to allow for create, update

--- app/admin/subscription.rb
+++ app/admin/subscription.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Subscription do
+  permit_params :plan_name, :billing_cycle, :payment_form
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
