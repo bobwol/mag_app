e09d0a347182db6b1d90189471b2bde5a4cfdab3
Prelang Builder <builder@prelang.com>
2016-04-14 02:19:47 +0000

    Defined validation for attribute 'googleplay_url' on model 'App'

--- app/models/app.rb
+++ app/models/app.rb
@@ -4,4 +4,5 @@ class App < ActiveRecord::Base
   has_one :subscription
   has_one :banner
   validates_formatting_of :appstore_url, using: :url
+  validates_formatting_of :googleplay_url, using: :url
 end
