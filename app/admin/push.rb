cb42b9b44459d922563397bf07a3cc628484c59a
Prelang Builder <builder@prelang.com>
2016-04-14 02:20:15 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Push' to allow for create, update

--- app/admin/push.rb
+++ app/admin/push.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Push do
+  permit_params :push_message, :delivery_time, :push_app_id, :push_client_key, :push_restapi_key, :appstore_prod_cert, :appstore_dev_cert, :gcm_sender_id, :gcm_api_key
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
