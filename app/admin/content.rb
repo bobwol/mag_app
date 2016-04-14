0d90be0574d7c56ede059aca09a8a9abe2c6def5
Prelang Builder <builder@prelang.com>
2016-04-14 02:20:37 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Content' to allow for create, update

--- app/admin/content.rb
+++ app/admin/content.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Content do
+  permit_params :title, :info, :content_url, :content_price, :status, :product_id, :cover, :hpub_url, :content_id, :publish_date, :publish_platform
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
