28d2017c27b10223b452dc8aff6a620ba27ce961
Prelang Builder <builder@prelang.com>
2016-04-14 02:19:47 +0000

    Defined validation for attribute 'banner_image_url' on model 'Banner'

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,4 +2,5 @@ class Banner < ActiveRecord::Base
   belongs_to :app
   belongs_to :user
   validates_formatting_of :banner_url, using: :url
+  validates_formatting_of :banner_image_url, using: :url
 end
